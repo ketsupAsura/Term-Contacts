77b1443ba3bef7ed421aa0c0facef3f7c0bab682
ketsupAsura <alagabanmatt@gmail.com>
2023-11-04 03:48:56 -0900

    contact class

---
+++ src/Contact.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Contact.h"
+
+
+Contact::Contact(std::string name_, std::string phone_number_, std::string email_)
+: name(name_), phone_number(phone_number_), email(email_) {}
+
+std::string Contact::getName() const { 
+    return name; 
+}
+
+std::string Contact::getPhoneNumber() const { 
+    return phone_number;
+}
+
+std::string Contact::getEmail() const { 
+    return email;
+}
