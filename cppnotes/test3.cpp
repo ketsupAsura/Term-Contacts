db11f3664615c1de53a86c0694834ca9520f68a5
ketsupAsura <alagabanmatt@gmail.com>
2024-01-21 02:21:57 -0900

    Deleted some notes files

--- cppnotes/test3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-
-int main() {
-    int sum = 5 - 2 + 1;
-    std::cout << sum;
-    return 0;
-}
--- cppnotes/unicode.py
+++
@@ -1,4 +0,0 @@
-print("┌─ Contact List ─────────────────┐")
-print("│        This is awesome!        │")
-print("└────────────────────────────────┘")
-
